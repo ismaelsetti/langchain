0d32f671d1b35092956f8e1f535504464b780040
ismael <ismael.setti@ibm.com>
2024-04-16 16:24:05 +0200

    Add chatbot

---
+++ chatbot/locallama.py
@@ -0,0 +1,34 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## Langsmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]=os.getenv("LANGCHAIN_TRACING_V2")
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+## Prompt Template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question: {question}")
+    ]
+)
+
+## streamlit framework
+
+st.title("Langchain Demo With LLAMA 2 API")
+input_text = st.text_input("Search the topic you want")
+
+## ollama LLM
+llm = Ollama(model="llama2")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({'question': input_text}))
